37136555acd255134a65731a3ccebee3a59f71dd
Victor Jimenez <vjimenez@americatv.com.pe>
2017-01-12 16:09:22 -0500

    linkq

---
+++ MvcLinqSql/Controllers/AreaController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLinqSql.Models;
+
+namespace MvcLinqSql.Controllers
+{
+    public class AreaController : Controller
+    {
+        //
+        // GET: /Area/
+        public ModelArea model = new ModelArea();
+
+
+        public ActionResult Index()
+        {
+            return View(model.ListarArea());
+        }
+
+       public ActionResult Datails(int id){
+        
+           var area = model.BuscarArea(id);
+             return View(area);  
+        }
+
+        public ActionResult Create(){
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Create(area a)
+        {
+
+        try{
+
+            if(model.InsertarArea(a).Equals("OK"))
+            {
+                return RedirectToAction("Index");
+            }else{
+            return RedirectToAction("Create");
+            }
+           }
+     catch{
+            return View();
+         }
+        }
+
+        public ActionResult Edit(int id) {
+            var area = model.BuscarArea(id);
+            return View(area);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, area a) {
+
+            try
+            {
+                area area = new area();
+                area.area_cod = id;
+                area.area_des = a.area_des;
+
+                if (model.ActualizarArea(area).Equals("OK"))
+                {
+
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    return RedirectToAction("Edit");
+                }
+            }
+            catch {
+
+                return View();
+            
+            }
+        
+        
+        }
+
+        public ActionResult Delete(int id) {
+
+            var area = model.BuscarArea(id);
+            return View(area);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection) {
+
+            try {
+                if (model.eliminarArea(id).Equals("OK"))
+                {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    return RedirectToAction("Delete");
+                }
+
+            }
+            catch {
+                return View();
+            }
+        
+        }
+
+
+    }
+}
---
+++ MvcLinqSql/Models/ModelArea.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLinqSql.Models
+{
+    public class ModelArea
+    {
+        DataEmpresaDataContext contexto = new DataEmpresaDataContext();
+
+        public List<area> ListarArea() {
+        List<area> lista = new List<area>();
+            var consulta = contexto.PR_LISTAR_AREA();
+
+            foreach(var area in consulta){
+            area a = new area();
+                a.area_cod= area.area_cod;
+                a.area_des = area.area_des;
+                lista.Add(a);
+                      
+            }
+         return lista;
+        }
+
+        public area BuscarArea(int codigo)
+        {
+
+            area a = new area();
+            try
+            {
+                var consulta = contexto.PR_BUSCAR_AREA(codigo);
+                foreach (var area in consulta)
+                {
+                    a.area_cod = area.area_cod;
+                    a.area_des = area.area_des;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return a;
+        }
+
+        public string InsertarArea(area a)
+        {
+
+            string resultado = String.Empty;
+
+            try
+            {
+                contexto.PR_INSERTAR_AREA(a.area_des);
+                resultado = "OK";
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+
+            }
+
+            return resultado;
+        }
+
+        public String ActualizarArea(area a)
+        {
+            String resultado = String.Empty;
+            try
+            {
+                contexto.PR_ACTUALIZAR_AREA(a.area_cod, a.area_des);
+                resultado = "OK";
+
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+
+            }
+            return resultado;
+
+
+        }
+
+        public string eliminarArea(int codigo)
+        {
+            string resultado = String.Empty;
+
+            try
+            {
+                contexto.PR_ELIMINAR_AREA(codigo);
+                resultado = "OK";
+            }
+            catch (Exception ex)
+            {
+                resultado = ex.Message;
+            }
+
+            return resultado;
+        }
+
+    }
+}
